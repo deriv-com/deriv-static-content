77e733b1313acf35fc49f0d87c96a5a773d3839f
Sash <sash@binary.com>
2019-10-28 16:30:42 +0200

    update label

---
+++ cashier/js/test-validation.js
@@ -0,0 +1,45 @@
+const FloatLabel = (() => {
+
+	// add active class
+	const handleFocus = (e) => {
+		const target = e.target;
+		target.parentNode.classList.add('active');
+		target.setAttribute('placeholder', target.getAttribute('data-placeholder'));
+	};
+
+	// remove active class
+	const handleBlur = (e) => {
+		const target = e.target;
+		if (!target.value) {
+			target.parentNode.classList.remove('active');
+		}
+		target.removeAttribute('placeholder');
+	};
+
+	// register events
+	const bindEvents = (element) => {
+		const floatField = element.querySelector('input');
+		floatField.addEventListener('focus', handleFocus);
+		floatField.addEventListener('blur', handleBlur);
+	};
+
+	// get DOM elements
+	const init = () => {
+		const floatContainers = document.querySelectorAll('.float-container');
+
+		floatContainers.forEach((element) => {
+
+			if (element.querySelector('input').value) {
+				element.classList.add('active');
+			}
+
+			bindEvents(element);
+		});
+	};
+
+	return {
+		init: init
+	};
+})();
+
+FloatLabel.init();
