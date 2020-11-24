21434263437564b31eec92b6e7db898e2bb8f592
ci-build <ci-build@binary.com>
2020-11-24 03:23:57 +0000

    [skip ci]

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -7,12 +7,13 @@ function getCookie(name) {
   const dc = document.cookie;
   const prefix = name + "=";
   let begin = dc.indexOf("; " + prefix);
+  let end = null
   if (begin == -1) {
     begin = dc.indexOf(prefix);
     if (begin != 0) return null;
   } else {
     begin += 2;
-    let end = document.cookie.indexOf(";", begin);
+    end = document.cookie.indexOf(";", begin);
     if (end == -1) {
       end = dc.length;
     }
