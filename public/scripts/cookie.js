7bd51c21530b5ca5c33520b13bcddd4eb2140151
Prince <prince@deriv.com>
2022-11-08 14:26:26 +0400

    chore: updated affiliate_tracking cookie expiration

--- public/scripts/cookie.js
+++ public/scripts/cookie.js
@@ -57,27 +57,43 @@ const toISOFormat = (date) => {
 
 const shouldOverwrite = (new_utm_data, current_utm_data) => {
   if (!current_utm_data) {
-      return true;
+    return true;
   } else if (!new_utm_data) {
-      return false;
+    return false;
   }
-  
+
   // Check if both new and old utm_data has all required fields
-  const required_fields = ['utm_source', 'utm_medium', 'utm_campaign'];
-  const has_new_required_fields = required_fields.every((field) => new_utm_data[field]);
-  const has_curr_required_fields = required_fields.every((field) => current_utm_data[field]);
+  const required_fields = ["utm_source", "utm_medium", "utm_campaign"];
+  const has_new_required_fields = required_fields.every(
+    (field) => new_utm_data[field]
+  );
+  const has_curr_required_fields = required_fields.every(
+    (field) => current_utm_data[field]
+  );
 
   // Overwrite based on the order of priority
   if (has_new_required_fields && has_curr_required_fields) {
-      if (new_utm_data.utm_medium.includes('aff')) return true; // 1. Affiliate tags
-      else if (new_utm_data.utm_medium.includes('ppc') && !current_utm_data.utm_medium.includes('aff')) return true; // 2. PPC tags
-      else if (!current_utm_data.utm_medium.includes('ppc') && !current_utm_data.utm_medium.includes('aff')) return true; // 3. Complete set of required tags
+    if (new_utm_data.utm_medium.includes("aff"))
+      return true; // 1. Affiliate tags
+    else if (
+      new_utm_data.utm_medium.includes("ppc") &&
+      !current_utm_data.utm_medium.includes("aff")
+    )
+      return true; // 2. PPC tags
+    else if (
+      !current_utm_data.utm_medium.includes("ppc") &&
+      !current_utm_data.utm_medium.includes("aff")
+    )
+      return true; // 3. Complete set of required tags
   } else if (has_new_required_fields) {
-      return true;
+    return true;
   } else if (has_curr_required_fields) {
-      return false;
-  } else if (new_utm_data.utm_source !== undefined
-      && Object.values(new_utm_data).length >= Object.values(current_utm_data).length) return true; // 4. Everything else
+    return false;
+  } else if (
+    new_utm_data.utm_source !== undefined &&
+    Object.values(new_utm_data).length >= Object.values(current_utm_data).length
+  )
+    return true; // 4. Everything else
   return false;
 };
 /* end utility functions */
@@ -105,7 +121,7 @@ const shouldOverwrite = (new_utm_data, current_utm_data) => {
 
   // If the user comes to the site for the first time without any URL params
   // Only set the utm_source to referrer if the user does not have utm_data cookies stored
-  utm_data["utm_source"] = document.referrer? document.referrer: "null";
+  utm_data["utm_source"] = document.referrer ? document.referrer : "null";
 
   // If the user has any new UTM params, store them
   utm_fields.forEach((field) => {
@@ -135,7 +151,9 @@ const shouldOverwrite = (new_utm_data, current_utm_data) => {
   /* start handling affiliate tracking */
   if (searchParams.has("t")) {
     eraseCookie("affiliate_tracking");
-    document.cookie = `affiliate_tracking=${searchParams.get("t")};domain=${getDomain()}; path=/; SameSite=None; Secure;`;
+    document.cookie = `affiliate_tracking=${searchParams.get(
+      "t"
+    )}; expires=Tue, 19 Jan 9999 03:14:07 UTC;  domain=${getDomain()}; path=/; SameSite=None; Secure;`;
   }
   /* end handling affiliate tracking */
 
