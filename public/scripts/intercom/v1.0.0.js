8940026466d2d21b653a6b4609e1626374a2df28
Nuzhy-Deriv <nuzhy@deriv.com>
2024-11-18 16:13:31 +0800

    fix: remove console logs

--- public/scripts/intercom/v1.0.0.js
+++ public/scripts/intercom/v1.0.0.js
@@ -1,8 +1,6 @@
 class DerivInterCom {
   constructor({ userData = null, hideLauncher = true } = {}) {
-    console.log(userData);
     this.userData = userData;
-    console.log(hideLauncher);
 
     this.intercomConfig = {
       app_id: "rfwdy059",
@@ -38,11 +36,9 @@ class DerivInterCom {
             (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rfwdy059';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
         `;
     document.body.appendChild(script);
-    console.log('here')
 
     window.isInterComExists = true;
     Intercom("boot", this.intercomConfig);
-    
   };
 }
 
